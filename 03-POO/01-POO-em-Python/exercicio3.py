231d02ff1a78c3ed53a8c76a6aa45b16426ce10e
Andre Felipe <afelipes@gmail.com>
2023-10-26 17:01:46 -0300

    feat: Classes de objetos

---
+++ 03-POO/01-POO-em-Python/exercicio3.py
@@ -0,0 +1,8 @@
+'''
+Exercício 3
+Crie a classe Database. Não precisa colocar nada dentro dela, somente a palavra reservada pass.
+'''
+
+
+class Database:
+    pass
---
+++ 03-POO/01-POO-em-Python/exercicio4.py
@@ -0,0 +1,11 @@
+'''
+Exercício 4
+Crie dois objetos da classe Database, criada no exercício 
+anterior, instanciando-os.
+'''
+
+import exercicio3
+
+
+db1 = exercicio3.Database()
+db2 = exercicio3.Database()
